923ebd62fcc03e1a27bbb90936a03bab8a479def
Gábor Németh <ngd666@gmail.com>
2020-04-22 21:48:58 +0200

    Set scrollable main-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {TopMenuComponent} from './top-menu/top-menu.component';
 import {SideMenuComponent} from './side-menu/side-menu.component';
-import {MainContentComponent} from './main-content/main-content.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import { BreweryListPageComponent } from './brewery-list-page/brewery-list-page.component';
 
@@ -14,7 +13,6 @@ import { BreweryListPageComponent } from './brewery-list-page/brewery-list-page.
     AppComponent,
     TopMenuComponent,
     SideMenuComponent,
-    MainContentComponent,
     HomePageComponent,
     BreweryListPageComponent
   ],
